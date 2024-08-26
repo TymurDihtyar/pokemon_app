e654d1b0ecb720b0cc84a746df0fcd3fdbd2afac
Tymur1990 <89634574+Tymur1990@users.noreply.github.com>
2024-08-26 14:06:04 +0300

    add search

--- src/pages/CollectionPage.tsx
+++ src/pages/CollectionPage.tsx
@@ -1,7 +1,7 @@
 import {OnePokemon} from "../components/PokemonsContainer/OnePokemon";
 
 const CollectionPage = () => {
-    const myCollection = JSON.parse(localStorage.getItem("myCollection"));
+    const myCollection = JSON.parse(localStorage.getItem("myCollection")) || [];
     return (
         <div className="bg-gradient-to-bl from-indigo-300 via-blue-300 to-purple-300 min-h-screen pt-10">
             <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 lg:max-w-7xl lg:px-8">
